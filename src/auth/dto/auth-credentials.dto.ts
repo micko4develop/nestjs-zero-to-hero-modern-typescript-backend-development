ad63c49577a17f4ea48d3196b19a4b80d93a88da
Milovan Marković <milovan0807@gmail.com>
2025-08-22 18:21:49 +0200

    changes for messages

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,16 @@
+import { IsString, Length, Matches, Max, MaxLength, MinLength } from "class-validator";
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(8, { message: 'Username must be at least 8 characters long!' })
+  @MaxLength(20, { message: 'Username must be at most 20 characters long!' })
   username: string;
+
+  @IsString()
+  @MinLength(8, { message: 'Password must be at least 8 characters long!' })
+  @MaxLength(32, { message: 'Password must be at most 32 characters long!' })
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/, {
+    message: 'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character!',
+  })
   password: string;
 }
